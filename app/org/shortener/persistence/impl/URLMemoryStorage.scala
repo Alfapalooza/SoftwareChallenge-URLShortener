bf0c7d46e8dd3274b3716bc1e78b6dd62d2a07f7
John Doe <mallaire77@gmail.com>
2018-07-12 19:57:05 -0400

    Breaking out update in to it's own synchronized function

--- app/org/shortener/persistence/impl/URLMemoryStorage.scala
+++ app/org/shortener/persistence/impl/URLMemoryStorage.scala
@@ -16,34 +16,31 @@ class URLMemoryStorage extends URLStorage {
   private lazy val urls =
     new DualHashBidiMap[Token, URL]
 
-  override def upsert(url: URL)(implicit ec: ExecutionContext): Future[Token] = {
-    Future {
-      Option(urls.getKey(url))
-        .getOrElse {
-          synchronized {
-            val newToken =
-              generateAvailableToken
-
-            urls
-              .put(newToken, url)
-
-            newToken
-          }
-        }
-    }
-  }
+  override def upsert(url: URL)(implicit ec: ExecutionContext): Future[Token] =
+    Future(Option(urls.getKey(url)).getOrElse(handleUpdate(url)))
 
   override def fetch(token: Token)(implicit ec: ExecutionContext): Future[URL] =
     Future(Option(urls.get(token)).getOrElse(throw URLNotFoundException(token)))
 
-  @tailrec
-  private def generateAvailableToken: Token = {
-    val newToken =
-      UUID.randomUUID.toString.split("-").head
+  protected def handleUpdate(url: URL): Token =
+    synchronized {
+      @tailrec
+      def generateAvailableToken: Token = {
+        val newToken =
+          UUID.randomUUID.toString.split("-").head
 
-    if (Option(urls.get(newToken)).isDefined)
-      generateAvailableToken
-    else
+        if (Option(urls.get(newToken)).isDefined)
+          generateAvailableToken
+        else
+          newToken
+      }
+
+      val newToken =
+        generateAvailableToken
+
+      urls
+        .put(newToken, url)
+
       newToken
-  }
+    }
 }
